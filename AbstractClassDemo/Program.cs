e31cc027307628e6148d5dbb0da52294cc063d7d
NathanielAddo <stevenelly100@gmail.com>
2025-08-08 21:25:05 +0000

    Create AbstractClassDemo Project

---
+++ AbstractClassDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AbstractClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Abstract Classes and Methods ===");
+            
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(4, 6);
+            
+            Console.WriteLine($"Circle area: {circle.GetArea():F2}");
+            Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Height { get; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+}
