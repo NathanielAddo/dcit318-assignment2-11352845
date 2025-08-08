c301fc95f05b4a0f82e4ab383a1704521476f52b
NathanielAddo <stevenelly100@gmail.com>
2025-08-08 21:24:32 +0000

    Create Interface Project

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Interfaces ===");
+            
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+            
+            car.Move();
+            bicycle.Move();
+        }
+    }
+
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+}
